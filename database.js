0628a643d1b08d3dc5515fbf2a2a2143c59b1e24
kembiri <kembiri1@gmail.com>
2025-06-01 00:48:34 -0700

    Create database.js

---
+++ database.js
@@ -0,0 +1,12 @@
+const movies = [
+      { title: "Film 1", image: "https://image.tmdb.org/t/p/w500/wYDdWN1McB1Sio4z1dPSkb40Z78.jpg", url: "https://example.com/film1" },
+      { title: "Film 2", image: "https://image.tmdb.org/t/p/w500/wYDdWN1McB1Sio4z1dPSkb40Z78.jpg", url: "https://example.com/film2" },
+      { title: "Film 3", image: "https://image.tmdb.org/t/p/w500/wYDdWN1McB1Sio4z1dPSkb40Z78.jpg", url: "https://example.com/film3" },
+      { title: "Film 4", image: "https://image.tmdb.org/t/p/w500/wYDdWN1McB1Sio4z1dPSkb40Z78.jpg", url: "https://example.com/film4" },
+      { title: "Film 5", image: "https://image.tmdb.org/t/p/w500/wYDdWN1McB1Sio4z1dPSkb40Z78.jpg", url: "https://example.com/film5" },
+      { title: "Film 6", image: "https://image.tmdb.org/t/p/w500/wYDdWN1McB1Sio4z1dPSkb40Z78.jpg", url: "https://example.com/film6" },
+      { title: "Film 7", image: "https://image.tmdb.org/t/p/w500/wYDdWN1McB1Sio4z1dPSkb40Z78.jpg", url: "https://example.com/film7" },
+      { title: "Film 8", image: "https://image.tmdb.org/t/p/w500/wYDdWN1McB1Sio4z1dPSkb40Z78.jpg", url: "https://example.com/film8" },
+      { title: "Film 9", image: "https://image.tmdb.org/t/p/w500/wYDdWN1McB1Sio4z1dPSkb40Z78.jpg", url: "https://example.com/film9" },
+      { title: "Film 10", image: "https://image.tmdb.org/t/p/w500/wYDdWN1McB1Sio4z1dPSkb40Z78.jpg", url: "https://example.com/film10" }
+    ];
