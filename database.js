d48c20bab97151e70ac1e5ad8eff434be1d56102
kembiri <kembiri1@gmail.com>
2025-06-01 01:49:00 -0700

    Update database.js

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 const movies = [
       { title: "Flu (2013)", image: "https://m.media-amazon.com/images/M/MV5BMTQ4NzI2OTg5NV5BMl5BanBnXkFtZTgwNjQ3MDgyMjE@._V1_SX300.jpg", url: "https://example.com/film1" },
-      { title: "Kung Fu Hustle(2004)", image: "https://image.tmdb.org/t/p/w500/exbyTbrvRUDKN2mcNEuVor4VFQW.jpg", url: "https://example.com/film2" },
-      { title: "Shaolin Soccer(2001)", image: "https://image.tmdb.org/t/p/w500/1aOnEAg3FSIg8DncA9JvMBuARTZ.jpg", url: "https://example.com/film3" },
-      { title: "3 Idiots(2009)", image: "https://image.tmdb.org/t/p/w500/66A9MqXOyVFCssoloscw79z8Tew.jpg", url: "https://example.com/film4" },
+      { title: "Kung Fu Hustle (2004)", image: "https://image.tmdb.org/t/p/w500/exbyTbrvRUDKN2mcNEuVor4VFQW.jpg", url: "https://example.com/film2" },
+      { title: "Shaolin Soccer (2001)", image: "https://image.tmdb.org/t/p/w500/1aOnEAg3FSIg8DncA9JvMBuARTZ.jpg", url: "https://example.com/film3" },
+      { title: "3 Idiots (2009)", image: "https://image.tmdb.org/t/p/w500/66A9MqXOyVFCssoloscw79z8Tew.jpg", url: "https://example.com/film4" },
       { title: "Film 5", image: "https://image.tmdb.org/t/p/w500/wYDdWN1McB1Sio4z1dPSkb40Z78.jpg", url: "https://example.com/film5" },
       { title: "Film 6", image: "https://image.tmdb.org/t/p/w500/wYDdWN1McB1Sio4z1dPSkb40Z78.jpg", url: "https://example.com/film6" },
       { title: "Film 7", image: "https://image.tmdb.org/t/p/w500/wYDdWN1McB1Sio4z1dPSkb40Z78.jpg", url: "https://example.com/film7" },
